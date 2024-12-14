7be3e022956c7573838a23f4a0762df7e6cae9d4
João Vitor <joaovitorpessoal2@gmail.com>
2024-12-14 13:35:27 -0300

    Initial Commit

---
+++ ReminderTests/ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderTests.swift
+//  ReminderTests
+//
+//  Created by João Vitor Pereira on 14/12/24.
+//
+
+import Testing
+@testable import Reminder
+
+struct ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
