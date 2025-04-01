04b004422532e5ace1aff1279fa3dfed4ade4d55
joaovitor <joaovitorpessoal2@gmail.com>
2025-04-01 00:38:20 -0300

    add view from bottom sheet

--- Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
+++ Reminder/Sources/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
@@ -10,18 +10,50 @@ import UIKit
 
 class LoginBottomSheetViewController: UIViewController {
     let loginView = LoginBottomSheetView()
+    var handleAreaHeight: CGFloat = 50.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        setupGesture() // vai configurar a animação da bottom sheet
     }
     
     private func setupUI() {
+        self.view.addSubview(loginView)
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
         setupConstraints()
     }
     
     private func setupConstraints() {
-        self.view.addSubview(loginView)
+        NSLayoutConstraint.activate([
+            loginView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            loginView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            loginView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+        let heightConstraint = loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5).isActive = true
     }
+    
+    private func setupGesture() {
+        
+    }
+    
+    private func handlePanGesture() {
+        
+    }
+    
+    func animateShow(completion: (() -> Void)? = nil) {
+        self.view.layoutIfNeeded()
+        loginView.transform = CGAffineTransform(translationX: 0, y: loginView.frame.height)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.loginView.transform = .identity
+            self.view.layoutIfNeeded()
+        }) { _ in
+            completion?()
+        }
+    }
+    
+   
 }
--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -23,6 +23,7 @@ class SplashViewController: UIViewController {
         self.view.backgroundColor = Colors.primaryRedBase
         
         setupConstrainsts()
+        setupGesture()
     }
     
     private func setupConstrainsts() {
@@ -34,5 +35,20 @@ class SplashViewController: UIViewController {
         ])
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showLoginBottomSheet() {
+        let loginBottomSheet = LoginBottomSheetViewController()
+        loginBottomSheet.modalPresentationStyle = .overCurrentContext
+        loginBottomSheet.modalTransitionStyle = .crossDissolve
+        self.present(loginBottomSheet, animated: false) {
+            loginBottomSheet.animateShow()
+        }
     }
 }
